aa4c93f876395c0cecc9c6bb9214f7d6a5c8b901
Chris G <chris@saturn.earth>
2023-08-05 19:24:11 -0500

    This application is now complete.

---
+++ UdpServer/buildApp.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+g++ -o udpServer -I include src/udpServer.cc src/UdpServer.cc
+
--- UdpServer/src/udpServer.cc
+++ UdpServer/src/udpServer.cc
@@ -7,32 +7,173 @@
 #include <string.h>
 #include "UdpServer.h"
 
+// This structure is used to consolidate user parameters.
+struct MyParameters
+{
+  int *listenPortPtr;
+  int *maxPayloadLengthPtr;
+};
 
+/**************************************************************************
+
+  Name: receiveCallback
+
+  Purpose: The purpose of this function is to serve as the constructor
+  of a UdpServer object.  This function opens a socket, and if
+  successful, populates the peer address structure with the appropriate
+  information.
+
+  Calling Sequence: receiveCallback(peerAddressPtr,
+                                    bufferPtr,
+                                    bufferLength)
+
+  Inputs:
+
+    peerAddressPtr - A pointer to a sockaddr structure so that replies
+    could be sent to the link partner.
+
+    bufferPtr - A pointer to a buffer of received data.
+
+    bufferLength - The number of bytes int the buffer that is
+    referenced by bufferPtr.
+
+  Outputs:
+
+    None.
+
+**************************************************************************/
 void receiveCallback(sockaddr *peerAddressPtr,
                      void *bufferPtr,
                      uint32_t bufferLength)
 {
 
-  printf("Buffer Length: %u\n",bufferLength);
-  printf("%s",bufferPtr);
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // Write the buffer to stdout so that the data can be piped to
+  // a different program.
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  fwrite(bufferPtr,sizeof(unsigned char),bufferLength,stdout);
 
   return;
 
 } // receiveCallback
 
+/*****************************************************************************
 
+  Name: getUserArguments
+
+  Purpose: The purpose of this function is to retrieve the user arguments
+  that were passed to the program.  Any arguments that are specified are
+  set to reasonable default values.
+
+  Calling Sequence: exitProgram = getUserArguments(parameters)
+
+  Inputs:
+
+    parameters - A structure that contains pointers to the user parameters.
+
+  Outputs:
+
+    exitProgram - A flag that indicates whether or not the program should
+    be exited.  A value of true indicates to exit the program, and a value
+    of false indicates that the program should not be exited..
+
+*****************************************************************************/
+bool getUserArguments(int argc,char **argv,struct MyParameters parameters)
+{
+  bool exitProgram;
+  bool done;
+  int opt;
+
+  // Default not to exit program.
+  exitProgram = false;
+
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // Default parameters.
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // Default a resonable port.
+  *parameters.listenPortPtr = 8001;
+
+  // Default to 2048 octets.
+  *parameters.maxPayloadLengthPtr = 2048;
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+  // Set up for loop entry.
+  done = false;
+
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // Retrieve the command line arguments.
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  while (!done)
+  {
+    // Retrieve the next option.
+    opt = getopt(argc,argv,"p:m:h");
+
+    switch (opt)
+    {
+      case 'p':
+      {
+        *parameters.listenPortPtr = atoi(optarg);
+        break;
+      } // case
+
+      case 'm':
+      {
+        *parameters.maxPayloadLengthPtr = atoi(optarg);
+        break;
+      } // case
+
+      case 'h':
+      {
+        // Display usage.
+        fprintf(stderr,"./udpClient -p <listenport> -m <maxpayloadlength>\n");
+
+        // Indicate that program must be exited.
+        exitProgram = true;
+        break;
+      } // case
+
+      case -1:
+      {
+        // All options consumed, so bail out.
+        done = true;
+      } // case
+    } // switch
+
+  } // while
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+  return (exitProgram);
+
+} // getUserArguments
+
+//*************************************************************************
+// Mainline code.
+//*************************************************************************
 int main(int argc,char **argv)
 {
+  bool exitProgram;
+  int listenPort;
+  int maxPayloadLength;
   UdpServer *networkInterfacePtr;
+  struct MyParameters parameters;
 
-  if (argc != 2)
+ // Set up for parameter transmission.
+  parameters.listenPortPtr = &listenPort;
+  parameters.maxPayloadLengthPtr = &maxPayloadLength;
+
+  // Retrieve the system parameters.
+  exitProgram = getUserArguments(argc,argv,parameters);
+
+  if (exitProgram)
   {
-    fprintf(stderr,"Usage: %s  <port>\n",argv[0]);
-    return (-1);
+    // Bail out.
+    return (0);
   } // if
 
   // Create UDP server object.
-  networkInterfacePtr = new UdpServer(atoi(argv[1]),2048,receiveCallback);
+  networkInterfacePtr = new UdpServer(listenPort,
+                                      maxPayloadLength,
+                                      receiveCallback);
 
   if (!networkInterfacePtr->connectionIsEstablished())
   {
@@ -44,7 +185,10 @@ int main(int argc,char **argv)
   networkInterfacePtr->receiveData();
 
   // Clean up.
-  delete networkInterfacePtr;
+  if (networkInterfacePtr != NULL)
+  {
+    delete networkInterfacePtr;
+  } // if
 
   return (0);
 
